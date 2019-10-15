922b83ff7ecdfb59a7752c58fc54ee27d49489fe
Michael Brewer <m13rewer13@gmail.com>
2019-10-14 17:58:57 -0700

    stuff works

--- prep-discovery/src/main/java/com/michael/app/prep_discovery/PrepDiscoveryServiceApp.java
+++ prep-discovery/src/main/java/com/michael/app/prep_discovery/PrepDiscoveryServiceApp.java
@@ -2,7 +2,13 @@ package com.michael.app.prep_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,4 +16,15 @@ public class PrepDiscoveryServiceApp {
 	public static void main(String[] args) {
 		SpringApplication.run(PrepDiscoveryServiceApp.class, args);
 	}
+	
+	@Bean
+	@Profile("!default")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+	  EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+	  AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	  b.setDataCenterInfo(info);
+	  return b;
+	}
 }
+
+
